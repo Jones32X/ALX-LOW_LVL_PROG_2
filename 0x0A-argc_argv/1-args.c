e990a6ab7590521bd504408f4244a740e2480af9
Jones32X <jonespedro.Nezingu.com>
2023-10-14 23:25:49 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - responsible for printing program name
+ * @argc: amountof arguments
+ * @argv: argument array
+ * Return: Alwasys 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", *argv);
+	return (0);
+}
